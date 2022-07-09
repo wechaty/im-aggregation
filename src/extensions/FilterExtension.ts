058b518d79d160c707fc73fce9b53362d84eaec6
TankNee <nee@tanknee.cn>
2022-07-09 17:45:51 +0800

    :sparkles: complete the filter ext.

--- src/extensions/FilterExtension.ts
+++ src/extensions/FilterExtension.ts
@@ -3,7 +3,7 @@ import {
     getAllConfigurations,
     setConfiguration,
 } from "../database/impl/configuration";
-import { FilterType } from "../schema/types";
+import { FilterType, SimpleAccountProfile } from "../schema/types";
 import Extension from "./Extension";
 
 export default class FilterExtension extends Extension {
@@ -32,7 +32,111 @@ export default class FilterExtension extends Extension {
         };
     }
 
+    appendBlacklist() {
+        const handle = async (alias: string) => {
+            const contact = await this.adapter.bot.Contact.find({ alias });
+            if (!contact) {
+                this.adapter.bot.say(`No contact found for alias ${alias}`);
+                return;
+            }
+            const config = getAllConfigurations();
+            const profile: SimpleAccountProfile = {
+                source: this.adapter.profile.source,
+                id: contact.id,
+                name: contact.name(),
+                alias,
+            };
+            config.blacklist.push(profile);
+            setConfiguration(config);
+            await this.adapter.bot.say(`Added ${profile.name} to blacklist`);
+        };
+        return {
+            name: "Append Blacklist",
+            description: "Append a user to blacklist.",
+            shortcut: "ab",
+            handle,
+        };
+    }
+
+    removeBlacklist() {
+        const handle = async (alias: string) => {
+            const config = getAllConfigurations();
+            const index = config.blacklist.findIndex(
+                (p) =>
+                    p.source === this.adapter.profile.source &&
+                    p.alias === alias
+            );
+            if (index === -1) {
+                this.adapter.bot.say(`No contact found for alias ${alias}`);
+                return;
+            }
+            config.blacklist.splice(index, 1);
+            setConfiguration(config);
+            await this.adapter.bot.say(`Removed ${alias} from blacklist`);
+        };
+        return {
+            name: "Remove Blacklist",
+            description: "Remove a user from blacklist.",
+            shortcut: "rb",
+            handle,
+        };
+    }
+
+    appendWhitelist() {
+        const handle = async (alias: string) => {
+            const contact = await this.adapter.bot.Contact.find({ alias });
+            if (!contact) {
+                this.adapter.bot.say(`No contact found for alias ${alias}`);
+                return;
+            }
+            const config = getAllConfigurations();
+            const profile: SimpleAccountProfile = {
+                source: this.adapter.profile.source,
+                id: contact.id,
+                name: contact.name(),
+                alias,
+            };
+            config.whitelist.push(profile);
+            setConfiguration(config);
+            await this.adapter.bot.say(`Added ${profile.name} to whitelist`);
+        };
+        return {
+            name: "Append Whitelist",
+            description: "Append a user to whitelist.",
+            shortcut: "aw",
+            handle,
+        };
+    }
+
+    removeWhitelist() {
+        const handle = async (alias: string) => {
+            const config = getAllConfigurations();
+            const index = config.whitelist.findIndex(
+                (p) =>
+                    p.source === this.adapter.profile.source &&
+                    p.alias === alias
+            );
+            if (index === -1) {
+                this.adapter.bot.say(`No contact found for alias ${alias}`);
+                return;
+            }
+            config.whitelist.splice(index, 1);
+            setConfiguration(config);
+            await this.adapter.bot.say(`Removed ${alias} from whitelist`);
+        };
+        return {
+            name: "Remove Whitelist",
+            description: "Remove a user from whitelist.",
+            shortcut: "rw",
+            handle,
+        };
+    }
+
     async register() {
         this.adapter.registerCommand(this.setFilterType());
+        this.adapter.registerCommand(this.appendBlacklist());
+        this.adapter.registerCommand(this.removeBlacklist());
+        this.adapter.registerCommand(this.appendWhitelist());
+        this.adapter.registerCommand(this.removeWhitelist());
     }
 }
