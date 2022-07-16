6e08ea4e94ef752b70084d3e761a55971a11779d
TankNee <nee@tanknee.cn>
2022-07-16 12:55:32 +0800

    :rocket: add config example file.

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ echo "Done."
 
 echo "Try to initialize the database..."
 ts-node src/database/sync.ts
-touch src/database/data.json
+mv src/database/config.json.example src/database/config.json
 echo "Done."
 
 echo "Please rename the .env.example to .env and fill in the values."
